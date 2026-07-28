3d8f53b3a9e5b21de37022b1d8c2e16c10ed09f6
Abdullah Arshad <57131829+abdullaharshadd@users.noreply.github.com>
2026-07-29 03:05:41 +0500

    fix: wire real routes into the entrypoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,22 +2,42 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
+	_ "github.com/lib/pq"
 	"github.com/rs/zerolog/log"
+
+	"migrated-app/internal/resources"
+	smartcontact "migrated-app/internal/smartcontact"
 )
 
 func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
+	cfg, err := resources.LoadConfig()
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to load config")
+	}
+
+	db, err := cfg.OpenDB(ctx, "postgres")
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to open database")
+	}
+	defer db.Close()
+
+	if err := resources.RunMigrations(ctx, db); err != nil {
+		log.Fatal().Err(err).Msg("failed to run migrations")
+	}
+
 	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: buildRouter(),
+		Addr:    fmt.Sprintf(":%d", cfg.ServerPort),
+		Handler: smartcontact.BuildRouter(db),
 	}
 
 	go func() {
@@ -26,7 +46,7 @@ func main() {
 		}
 	}()
 
-	log.Info().Msg("server started on :8080")
+	log.Info().Msgf("server started on %s", srv.Addr)
 	<-ctx.Done()
 
 	shutCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
