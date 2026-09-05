872ce951538bd896be543b632d373407980238a8
Abdullah Arshad <57131829+abdullaharshadd@users.noreply.github.com>
2026-09-06 04:52:34 +0500

    fix: server startup error — internal/smartcontact/repository/userdao.go

--- internal/smartcontact/repository/userdao.go
+++ internal/smartcontact/repository/userdao.go
@@ -1,25 +1,29 @@
 package repository
 
 import (
-	"migrated-app/internal/smartcontact/service"
+	"context"
+	"database/sql"
+	"errors"
 	"migrated-app/internal/smartcontact/model"
 	"github.com/jmoiron/sqlx"
+)
+
+var (
+	UserNotFoundError = errors.New("user not found")
 )
 
 type UserRepository struct {
 	db *sqlx.DB
 }
 
-func NewUserRepository(db *sqlx.DB) service.UserService {
-	return &UserRepository{db: db}
-}
-
-func (ur *UserRepository) CreateUser(user model.User) (model.User, error) {
-	// Create user logic
+func (ur *UserRepository) GetUserByID(ctx context.Context, id int) (*model.User, error) {
+	user := &model.User{}
+	query := "SELECT * FROM users WHERE id = $1"
+	err := ur.db.GetContext(ctx, user, query, id)
+	if err == sql.ErrNoRows {
+		return nil, UserNotFoundError
+	} else if err != nil {
+		return nil, err
+	}
 	return user, nil
-}
-
-func (ur *UserRepository) GetUser(id string) (model.User, error) {
-	// Get user logic
-	return model.User{}, nil
 }
