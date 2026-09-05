21d38090a8e054aa8d3c826f9b9a5f767c24fef4
Abdullah Arshad <57131829+abdullaharshadd@users.noreply.github.com>
2026-09-06 04:55:42 +0500

    fix: server startup error — internal/smartcontact/error/restresponseentityexceptionhandling.go

--- internal/smartcontact/error/restresponseentityexceptionhandling.go
+++ internal/smartcontact/error/restresponseentityexceptionhandling.go
@@ -13,8 +13,8 @@ func UserNotFoundMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		next.ServeHTTP(w, r)
 		if err := r.Context().Value("error"); err != nil {
-			if _, ok := err.(repository.UserNotFoundError); ok {
-				errMessage := map[string]string{"message": err.(repository.UserNotFoundError).Error(), "status": http.StatusText(http.StatusNotFound)}
+			if _, ok := err.(*repository.UserNotFoundError); ok {
+				errMessage := map[string]string{"message": "User not found", "status": http.StatusText(http.StatusNotFound)}
 				w.WriteHeader(http.StatusNotFound)
 				if err := json.NewEncoder(w).Encode(errMessage); err != nil {
 					log.Err(err).Msg("failed to write error response")
